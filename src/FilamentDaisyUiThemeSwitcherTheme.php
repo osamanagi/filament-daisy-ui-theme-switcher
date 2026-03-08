77be7ebc8b12ca9144a2776fa147f13ce91fedce
osamanagi <osamanagiworks@gmail.com>
2026-03-08 16:11:41 +0200

    refactor: remove skeleton package files and update to filament-daisy-ui-theme-switcher
    - Deleted old package files and configurations related to the skeleton package.
    - Updated README, composer.json, and other relevant files to reflect the new package name and description.
    - Removed unnecessary workflows and scripts, including PHPStan and build scripts.
    - Added new FilamentDaisyUiThemeSwitcher class to implement the plugin functionality.
    - Updated test cases and namespaces to align with the new package structure.

--- src/FilamentDaisyUiThemeSwitcherTheme.php
+++ src/FilamentDaisyUiThemeSwitcherTheme.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton;
+namespace Osamanagi\FilamentDaisyUiThemeSwitcher;
 
 use Filament\Contracts\Plugin;
 use Filament\Panel;
@@ -8,17 +8,17 @@ use Filament\Support\Assets\Theme;
 use Filament\Support\Color;
 use Filament\Support\Facades\FilamentAsset;
 
-class Skeleton implements Plugin
+class FilamentDaisyUiThemeSwitcher implements Plugin
 {
     public function getId(): string
     {
-        return 'skeleton';
+        return 'filament-daisy-ui-theme-switcher';
     }
 
     public function register(Panel $panel): void
     {
         FilamentAsset::register([
-            Theme::make('skeleton', __DIR__ . '/../resources/dist/skeleton.css'),
+            Theme::make('filament-daisy-ui-theme-switcher', __DIR__ . '/../resources/dist/filament-daisy-ui-theme-switcher.css'),
         ]);
 
         $panel
@@ -29,7 +29,7 @@ class Skeleton implements Plugin
             ->dangerColor(Color::Rose)
             ->successColor(Color::Green)
             ->grayColor(Color::Gray)
-            ->theme('skeleton');
+            ->theme('filament-daisy-ui-theme-switcher');
     }
 
     public function boot(Panel $panel): void
